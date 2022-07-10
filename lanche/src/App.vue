3056e1fa97fd9f43114ae948591373d6f729964b
Eduardo Messias <messiasgit@outlook.com>
2022-07-10 13:25:32 -0300

    inital commit

---
+++ lanche/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Montar />
+</template>
+
+<script>
+import Montar from './components/Montar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Montar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ lanche/src/components/Montar.vue
@@ -0,0 +1,62 @@
+<template>
+  <nav>
+    <div class="container">
+        <span class="navbar"> <h1>Monte seu lanche</h1></span>
+    </div>
+  </nav>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+            Imagens do lanche
+      </div>
+
+      <div class="col-md-6">
+          <div class="row">
+            <div class="col">
+              <div class="card">
+                <div class="card-header"> Tipo do Pao</div>
+                <div class="card-body"></div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row mt-2">
+            <div class="col">
+              <div class="card">
+                <div class="card-header"> Salada</div>
+                <div class="card-body"></div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row mt-2">
+            <div class="col">
+              <div class="card">
+                <div class="card-header"> Molhos</div>
+                <div class="card-body"></div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row mt-2">
+            <div class="col">
+              <div class="card">
+                <div class="card-header"> Hamburger</div>
+                <div class="card-body"></div>
+              </div>
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Montar'
+}
+</script>
+
+<style>
+
+</style>
